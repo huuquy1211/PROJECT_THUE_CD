a68a677adcd5e1182fe16f0cb6ee1ddd8ef3ac4e
unknown <huuquy1211@gmail.com>
2019-06-20 23:33:08 +0700

    commit

--- THUE_CD/THUE_CD/Controllers/CustomerController.cs
+++ THUE_CD/THUE_CD/Controllers/CustomerController.cs
@@ -14,8 +14,30 @@ namespace THUE_CD.Controllers
         // GET: Customer
         public ActionResult IndexCustomer()
         {
-            var Cus = db.Customers.ToList();
-            return View(Cus);
+            List<Customer> Cus = db.Customers.ToList();
+            return View();
+
+            //var Cus = db.Customers.ToList();
+            //return View(Cus);
+        }
+
+        public JsonResult GetCustomerList()
+        {
+            List<Customer> CusList = db.Customers.Where(x => x.Fine == 0).Select(x => new Customer
+            {
+                Id_Customer = x.Id_Customer,
+                FullName = x.FullName,
+                Address = x.Address,
+                Phone = x.Phone,
+                Fine = x.Fine
+            }).ToList();
+            return Json(CusList, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult GetCustomer()
+        {
+            var Cus = db.Customers.OrderBy(a => a.Id_Customer).ToList();
+            return Json(new { data = Cus }, JsonRequestBehavior.AllowGet);
         }
         [HttpPost]
         public ActionResult UpdateCustomer(int id)
@@ -31,6 +53,7 @@ namespace THUE_CD.Controllers
                 return HttpNotFound();
             }
             return View(model);
+
         }
 
     }
