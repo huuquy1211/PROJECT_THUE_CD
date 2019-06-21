3bf6a05b700e4b6bca2c78132d47a50d4aa1e6af
unknown <huuquy1211@gmail.com>
2019-06-21 17:35:35 +0700

    commit

---
+++ THUE_CD/THUE_CD/ViewCustomer/ModelCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace THUE_CD.ViewCustomer
+{
+    public class ModelCustomer
+    {
+        public int Id_Customer { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public double Fine { get; set; }
+
+    }
+}
