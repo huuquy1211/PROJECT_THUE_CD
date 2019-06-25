22903d58d47c5f116142dc96aad57152c54fafa6
unknown <huuquy1211@gmail.com>
2019-06-25 23:33:19 +0700

    Quý 25/06

--- THUE_CD/THUE_CD/Controllers/ReportController.cs
+++ THUE_CD/THUE_CD/Controllers/ReportController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using THUE_CD.Models;
+using THUE_CD.ViewCustomer;
 
 namespace THUE_CD.Controllers
 {
+    
     public class ReportController : Controller
     {
+        ThueDiaDB db = new ThueDiaDB();
         // GET: Report
         public ActionResult IndexReportCustomer()
         {
@@ -18,5 +22,22 @@ namespace THUE_CD.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult GetCustomerList()
+        {
+            List<ModelCustomer> CusList = db.Customers.Select(x =>
+               new ModelCustomer
+               {
+                   Id_Customer = x.Id_Customer,
+                   Address = x.Address,
+                   Fine = x.Fine,
+                   FullName = x.FullName,
+                   Phone = x.Phone
+               }
+                ).ToList();
+            return Json(CusList, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
